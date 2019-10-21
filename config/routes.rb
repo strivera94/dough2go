a42cfa06a66faffab3e73b2500b206fc4b44ba17
Stephen Rivera <st.rivera_94@yahoo.com>
2019-10-21 17:38:37 -0400

    Database set and homepage and product views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get "/" , to:'static#index', as: 'home'
+  resources :product_orders
+  resources :products, only: [:index, :show]
+  resources :reviews
+  resources :orders
+  resources :customers, only:[:show, :new, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
