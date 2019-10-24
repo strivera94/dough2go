6244ab0efc7225f224b1a74b5e51dff136562287
github username <github email address>
2019-10-24 15:32:42 -0400

    Quick update to require_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,6 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    redirect_to home_path unless session.include? :user_id
+    redirect_to home_path unless session.include? :customer_id
   end
 end
